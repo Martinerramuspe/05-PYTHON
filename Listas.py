90f076f9cfc3913e2c206bca1e05ce9d94b67c59
Ing. Erramuspe Martin <109311548+Martinerramuspe@users.noreply.github.com>
2023-11-26 23:11:50 -0300

    Add files via upload

---
+++ Listas.py
@@ -0,0 +1,25 @@
+# LISTA 
+# Este tipo de objeto, si impporta el orde
+a= [1,2,"mario",4,"juan"]
+# Llamada de un elemento
+a[4]
+# Llamado de elementos consecutivos
+a[2:4]
+# Observacion_01: No se puede llamar 2 elementos o mas en una misma linea
+
+# Concatenado de listas
+lista1 = [1, 2, 3]
+lista2 = [4, 5, 6]
+
+lista3 = lista1 + lista2
+lista3
+
+# Diccionarios dentro de lista
+diccionario1 = {'nombre': 'Alice', 'edad': 30}
+diccionario2 = {'nombre': 'Bob', 'edad': 25}
+diccionario3 = {'nombre': 'Charlie', 'edad': 35}
+list = [diccionario1, diccionario2, diccionario3]
+list
+
+# Busqueda dentro de la lista, 
+a.index("juan")
